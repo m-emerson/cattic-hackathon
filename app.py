e3b871fcfcee769bbcabf4b084cd23c48e6427f6
mje <mje@catticbooks.(none)>
2015-08-08 23:52:01 +0000

    login changes

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask.ext.httpauth import HTTPBasicAuth
-from flask import render_template
+from flask import render_template, request, redirect
 
 import queries as db
 auth = HTTPBasicAuth()
 app = Flask(__name__)
+
+app.secret_key = 'omgsuchsecrets'
 
 @app.route("/")
 def hello():
@@ -25,6 +27,10 @@ def login():
 @app.route("/book")
 def book():
 	return render_template('book.html')
+
+@app.route("/images/book/<bookid>")
+def render_book_image(bookid):
+	return "render the book image by the id of the book"
 
 @auth.verify_password
 def verify_password(username, password):
