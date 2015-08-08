193df3a4b5667bf14f73897e6952847fa32303e0
root <root@catticbooks.(none)>
2015-08-08 03:37:46 +0000

    template for queries

---
+++ queries.py
@@ -0,0 +1,15 @@
+# here be the queries
+def get_books_by_courseid(courseid):
+	books = []
+	return books
+
+def get_course_by_courseid(courseid):
+	# Course information (name)
+	return true
+
+def search(search_query):
+	# e.g. GET COURSE LIKE "CSSE%"
+	return true
+
+def get_listings_for_book(bookid):
+	return true
