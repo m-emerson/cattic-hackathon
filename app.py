cffb53e6fe7a2ffb0136d3840e6d1fc87f8ab3f4
mje <mje@uq.edu.au>
2015-08-09 00:21:38 +0000

    change schema

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.secret_key = 'omgsuchsecrets'
 
 @app.route("/")
-def hello():
+def index():
 	return "Hello World!"
 
 @app.route("/test")
@@ -20,17 +20,34 @@ def test():
 def profile():
 	return jsonify({ 'data' : 'Hello %s!' % g.user.username })
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+	error = None
+	if request.method == 'POST':
+		user = db.authenticate_user(request.form['username'],
+					    request.form['password'])
+		if user:
+			session['username'] = request.form['username']
+			return redirect(url_for('index'))
+		else:
+			error = "Incorrect login details"
+	return render_template('login.html', error=error)
 
-@app.route("/book")
+@app.route("/book/<editionid>")
 def book():
 	return render_template('book.html')
 
-@app.route("/images/book/<bookid>")
+@app.route("/images/book/<editionid>")
 def render_book_image(bookid):
 	return "render the book image by the id of the book"
+
+@app.route("/courses/<courseid>")
+def course_books(courseid):
+	return "here is the course id"
+
+@app.route("/listings/create/<editionid>", methods=['GET', 'POST'])
+def create_listing():
+	return "woo"
 
 @auth.verify_password
 def verify_password(username, password):
