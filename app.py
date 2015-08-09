43e86277b636bb5e7fe0c6953a8975e377b15e75
mje <mje@uq.edu.au>
2015-08-09 04:27:16 +0000

    blah

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, send_file
+import io
 
-#import queries as db
+import queries as db
 app = Flask(__name__)
 
 app.secret_key = 'omgsuchsecrets'
@@ -21,14 +22,14 @@ def profile():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
 	error = None
-#	if request.method == 'POST':
-#		user = db.authenticate_user(request.form['username'],
-#					    request.form['password'])
-#		if user:
-#			session['username'] = request.form['username']
-#			return redirect(url_for('index'))
-#		else:
-#			error = "Incorrect login details"
+	if request.method == 'POST':
+		user = db.authenticate_user(request.form['username'],
+					    request.form['password'])
+		if user:
+			session['username'] = request.form['username']
+			return redirect(url_for('index'))
+		else:
+			error = "Incorrect login details"
 	return render_template('login.html', error=error)
 
 @app.route("/book/<editionid>")
@@ -43,12 +44,17 @@ def course():
 def about():
 	return render_template('about.html')
 
-@app.route("/images/book/<editionid>")
-def render_book_image(bookid):
-	return "render the book image by the id of the book"
+@app.route("/images/book/<isbn>")
+def render_book_image(isbn):
+	image_blob = db.get_book_image_by_isbn(isbn)
+	return send_file(io.BytesIO(image_blob))
 
 @app.route("/courses/<courseid>")
 def course_books(courseid):
+	course = db.get_course_by_courseid(courseid)
+	books = db.get_books_by_courseid(courseid)
+	return render_template('course.html', course=course, books=books);
+	print books
 	return "search books for this course"
 
 @app.route("/listings/create/<editionid>", methods=['GET', 'POST'])
